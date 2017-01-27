42bbf7f5bf2ca8be92d2f991177f17893d8efc52
HashiCorp Training <training@hashicorp.com>
2017-01-27 17:56:14 +0000

    initial state

---
+++ main.tf
@@ -0,0 +1,58 @@
+#
+# DO NOT DELETE THESE LINES!
+#
+# Your subnet ID is:
+#
+#     subnet-7e50c21a
+#
+# Your security group ID is:
+#
+#     sg-29ef374e
+#
+# Your AMI ID is:
+#
+#     ami-30217250
+#
+# Your Identity is:
+#
+#     autodesk-ape
+#
+
+variable "aws_access_key" {}
+
+variable "aws_secret_key" {}
+
+variable "aws_region" {
+  default = "us-west-1"
+}
+
+variable "web_count" {
+  default = "2"
+}
+
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "${var.aws_region}"
+}
+
+resource "aws_instance" web {
+  ami                    = "ami-30217250"
+  instance_type          = "t2.micro"
+  subnet_id              = "subnet-7e50c21a"
+  vpc_security_group_ids = ["sg-29ef374e"]
+  count                  = "${var.web_count}"
+
+  tags {
+    Identity = "autodesk.ape"
+
+    foo = "bar"
+
+    jb   = "test"
+    Name = "web ${count.index+1} / ${var.web_count}"
+  }
+}
+
+output "public_dns" {
+  value = ["${aws_instance.web.*.public_dns}"]
+}
